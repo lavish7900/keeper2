e8e6f328b683bdf75b2c1acadb0e45d75a64f006
lavish7900 <80806932+lavish7900@users.noreply.github.com>
2021-05-04 14:18:58 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map((x) => (
+        <Note key={x.key} title={x.title} content={x.content} />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
